41b87122681d34b574f6633ffd8d6baf036212d9
GYegor <yahor2011@gmail.com>
2019-02-20 00:51:30 +0300

    feat: add function getLoveTrianglesCount([])

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0, n, k, l;
+  for(var i = 0; i < preferences.length; i++) {
+    n = i + 1; 
+    k = preferences[i];
+    l = preferences[k - 1];
+    if( preferences[l -1] == n && n != k ) {
+        count++; delete preferences[i]; delete preferences[k - 1]; delete preferences[l -1];
+    }
+  }
+  return count;
 };
